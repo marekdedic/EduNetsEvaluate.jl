30d2b6397e6375695dc75cbe8e26ab111407321a
Marek Dedic <madedic@cisco.com>
2017-05-11 12:33:04 +0200

    More modular PRcurve Ctors

--- src/PRcurve/PRcurve.jl
+++ src/PRcurve/PRcurve.jl
@@ -18,6 +18,8 @@ function PRcurve(S4::PRcurveStage4, S5::PRcurveStage5)::PRcurve
 	return PRcurve(S4.thresholds, precision, S4.recall);
 end
 
+# Plotting
+
 function plotPRcurve(curve::PRcurve)
 	Plots.plot(curve.recall, curve.precision; xlabel = "Recall", ylabel = "Precision", xlims = (0, 1.05), ylims = (0, 1.05), label = "PR Curve");
 end
@@ -30,34 +32,34 @@ end
 # Complete PRcurve functions
 
 function PRcurve(model::EduNets.AbstractModel, positive::Vector{EduNets.AbstractDataset}, negative::Vector{EduNets.AbstractDataset}; thresholdCount::Int = 100)::PRcurve
-	function processS5(model::EduNets.AbstractModel, dataset::EduNets.AbstractDataset, S4::PRcurveStage4)::PRcurveStage5
-		state = EvaluationState(model, dataset);
-		return PRcurveStage5(S4, state);
-	end
-
 	statesPositive = map(p->EvaluationState(model, p), positive);
-	S1vec = map(s->PRcurveStage1(s), statesPositive);
+	statesNegative = map(n->EvaluationState(model, n), negative);
+	return PRcurve(statesPositive, statesNegative; thresholdCount = thresholdCount);
+end
+
+function PRcurve(positive::Vector{EvaluationState}, negative::Vector{EvaluationState}; thresholdCount::Int = 100)::PRcurve
+	S1vec = map(s->PRcurveStage1(s), positive);
 	S1 = vcat(S1vec...);
 	S2 = PRcurveStage2(S1, thresholdCount = thresholdCount);
-	S3vec = map(s->PRcurveStage3(S2, s), statesPositive);
+	S3vec = map(s->PRcurveStage3(S2, s), positive);
 	S3 = vcat(S3vec...);
 	S4 = PRcurveStage4(S3);
-	S5vec = map(n->processS5(model, n, S4), negative);
+	S5vec = map(n-PRcurveStage5(S4, n), negative);
 	S5 = vcat(S5vec...);
 	return PRcurve(S4, S5);
 end
 
 function PRcurve(model::EduNets.AbstractModel, positive::Vector{EduNets.AbstractDataset}; thresholdCount::Int = 100)::PRcurve
-	statesPositive = map(p->EvaluationState(model, p), positive);
-	S1vec = map(s->PRcurveStage1(s), statesPositive);
+		statesPositive = map(p->EvaluationState(model, p), positive);
+	return PRcurve(statesPositive; thresholdCount = thresholdCount);
+end
+
+function PRcurve{T<:AbstractFloat}(positive::Vector{EvaluationState{T}}; thresholdCount::Int = 100)::PRcurve
+	S1vec = map(s->PRcurveStage1(s), positive);
 	S1 = vcat(S1vec...);
 	S2 = PRcurveStage2(S1, thresholdCount = thresholdCount);
-	S3vec = map(s->PRcurveStage3(S2, s), statesPositive);
+	S3vec = map(s->PRcurveStage3(S2, s), positive);
 	S3 = vcat(S3vec...);
 	S4 = PRcurveStage4(S3);
 	return PRcurve(S4);
-end
-
-function PRcurve(model::EduNets.AbstractModel, dataset::EduNets.AbstractDataset; thresholdCount::Int = 100)::PRcurve
-	return PRcurve(model, [dataset]; thresholdCount = thresholdCount);
 end
