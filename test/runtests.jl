2f3bf83619c50f12c3e0835142119b0374f1acfe
Marek Dedic <madedic@cisco.com>
2017-05-11 14:12:38 +0200

    Added Full PR curve test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,18 @@ using Base.Test;
 
 include("testPRcurve.jl");
 
-@testset "PR curve" begin
-	for i in 1:10000
-		@test testPRcurve();
+@testset "All" begin 
+	@testset "PR curve" begin
+		@testset "Partial PR curve" begin
+			for i in 1:10000
+				@test testPRcurvePartial();
+			end
+		end
+
+		@testset "Full PR curve" begin
+			for i in 1:10000
+				@test testPRcurveFull();
+			end
+		end
 	end
 end
--- test/testPRcurve.jl
+++ test/testPRcurve.jl
@@ -3,7 +3,7 @@ using MLBase;
 using StatsBase;
 using Plots;
 
-function testPRcurve()
+function testPRcurvePartial()
 	real = rand(0:1, 1024);
 	predicted = rand(Float32, 1024);
 	rocvec = roc(real, predicted, nquantile(predicted, 100));
@@ -12,6 +12,27 @@ function testPRcurve()
 	real .+= 1;
 	es = EvaluationState(predicted, real);
 	pr = PRcurve([es]);
+	precisionResult = minimum((precisionML .- pr.precision) .< 0.01);
+	recallResult = minimum((recallML .- pr.recall) .< 0.01);
+
+	#plotPRcurve(pr);
+	#plot!(recallML, precisionML, label = "MLBase PR curve");
+	return precisionResult && recallResult;
+end
+
+function testPRcurveFull()
+	real1 = rand(0:1, 1024);
+	predicted1 = rand(Float32, 1024);
+	real2 = zeros(Int, 1024);
+	predicted2 = rand(Float32, 1024);
+	rocvec = roc(vcat(real1, real2), vcat(predicted1, predicted2), nquantile(predicted1, 100));
+	precisionML = map(r->precision(r), rocvec);
+	recallML = map(r->recall(r), rocvec);
+	real1 .+= 1;
+	real2 .+= 1;
+	es1 = EvaluationState(predicted1, real1);
+	es2 = EvaluationState(predicted2, real2);
+	pr = PRcurve([es1], [es2]);
 	precisionResult = minimum((precisionML .- pr.precision) .< 0.01);
 	recallResult = minimum((recallML .- pr.recall) .< 0.01);
 
